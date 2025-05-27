df67b477650bd17d2e77fa8cc611c430aa485e21
SofiaHervas <151835910+SofiaHervas@users.noreply.github.com>
2025-05-27 10:56:49 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='emotion_classifier',
+    version='0.1',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'torch',
+        'pandas',
+        'numpy',
+        'scikit-learn'
+    ],
+    entry_points={
+        'console_scripts': [
+            'inference=inference.cli:main'
+        ]
+    },
+)
